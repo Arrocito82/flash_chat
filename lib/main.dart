6e823f0f0a05637d92ef519bc70e13b3f92b4431
Andrea Monterrosa <moralesandrea.edr4@gmail.com>
2023-08-02 21:26:51 -0600

    Routes

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,6 @@
+import 'package:flash_chat/screens/chat_screen.dart';
+import 'package:flash_chat/screens/login_screen.dart';
+import 'package:flash_chat/screens/registration_screen.dart';
 import 'package:flash_chat/screens/welcome_screen.dart';
 import 'package:flutter/material.dart';
 
@@ -11,8 +14,19 @@ class FlashChat extends StatelessWidget {
         textTheme: TextTheme(
           bodyLarge: TextStyle(color: Colors.black54),
         ),
+        inputDecorationTheme: InputDecorationTheme(
+          hintStyle: TextStyle(
+            color: Colors.black54,
+          ),
+        ),
       ),
-      home: WelcomeScreen(),
+      initialRoute: 'login_screen',
+      routes: {
+        'chat_screen': (context) => ChatScreen(),
+        'login_screen': (context) => LoginScreen(),
+        'registration_screen': (context) => RegistrationScreen(),
+        'welcome_screen': (context) => WelcomeScreen()
+      },
       debugShowCheckedModeBanner: false,
     );
   }
